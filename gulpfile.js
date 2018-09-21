1440706ce378ea392c1babc84deba8721f3d2c55
rdrew <robdrewupei@gmail.com>
2018-09-21 13:13:27 -0300

    fixed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,89 @@
-var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var imagemin = require('gulp-imagemin'),
-    cache = require('gulp-cache');
+'use strict';
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+//var cache = require('gulp-cache');
+
+
+//===================
+// Browsersync Proxy
+//===================
 
 gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-       baseDir: "./"
-    }
-  });
+    browserSync.init({
+        proxy: "https://lmmijournal.researchspaces.ca",
+        serveStatic: ['.'],
+        files: ['./css/style.css','./js/js.js'],
+        plugins: ['bs-rewrite-rules'],
+        rewriteRules: [
+            {
+                match: '/themes/custom/lmmi_journal',
+                replace: ''
+            }
+        ]
+    });
 });
 
-gulp.task('bs-reload', function () {
-  browserSync.reload();
+
+//====================
+// Sass Compilation
+//===================
+
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'));
 });
+
+//====================
+// JS Concatination
+//===================
+
+gulp.task('js', function () {
+    gulp.src('./src/js/*.js') // path to your files
+    .pipe(concat('js.js'))  // concat and name it "concat.js"
+    .pipe(gulp.dest('./js/'));
+});
+
+//====================
+// Image Optimization
+//===================
 
 gulp.task('images', function(){
-  gulp.src('src/img/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('img/'));
+    gulp.src('src/img/**/*')
+    //.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(imagemin([
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('img/'));
 });
 
-gulp.task('styles', function(){
-  gulp.src(['src/scss/**/*.scss'])
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(sass())
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest('css/'))
-    .pipe(browserSync.reload({stream:true}))
+//==================
+// Default Gulp Task
+//=================
+
+gulp.task ('default',['browser-sync'], function(){
+    //watch sass folder and compile changes
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+    //watch js folder and compile changes
+    gulp.watch('src/js/**/*.js', ['js']);
+    //watch image folder and optimize
+    gulp.watch('src/img/**/*', ['images']);
 });
 
-gulp.task('scripts', function(){
-  return gulp.src('src/js/**/*.js')
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('js/'))
-    .pipe(browserSync.reload({stream:true}))
-});
 
-gulp.task('default', ['browser-sync'], function(){
-  gulp.watch("src/scss/**/*.scss", ['styles']);
-  gulp.watch("src/js/**/*.js", ['scripts']);
-  gulp.watch("*.html", ['bs-reload']);
-});
