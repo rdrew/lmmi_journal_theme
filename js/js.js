e550959a095b52d4702bd0140a7c2a6a69c5da91
rdrew <robdrewupei@gmail.com>
2018-10-03 13:48:17 -0300

    footnote popups

--- js/js.js
+++ js/js.js
@@ -1,14 +1,14 @@
 (function ($, Drupal) {
 
-  Drupal.behaviors.footnote_tooltips = {
-    attach: function attach(context, settings) {
-        //js goes here
-        //$('.menu').remove();
-        var footnote_link = $("a[id^='footnote']");
-         var footnote_link = $("li[id^='footnote']");
-
-    }
-  };
+    Drupal.behaviors.footnote_popups = {
+        attach: function attach(context, settings) {
+            $.each($("li[id^='footnote']"), function(index, value) {
+                var _footnoteText_ = $(value).text();
+                var _id_ = $(this).attr("id");
+                $('a[href$="' + _id_ + '"]').attr('data-toggle', 'tooltip').attr('data-original-title', _footnoteText_).attr('title', '');
+            });
+        }
+    };
 
 
 
