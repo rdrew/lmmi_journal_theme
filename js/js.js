2fefa4fa224832c0c54456a24170d0b1d328a831
rdrew <rdrew@upei.ca>
2020-10-15 15:20:05 -0300

    fixed toc js

--- js/js.js
+++ js/js.js
@@ -2,21 +2,6 @@
 
     Drupal.behaviors.footnote_popups = {
         attach: function attach(context, settings) {
-            /*
-                //target he footnotes list
-            $.each($(".footnote-list ul.footnotes a"), function(index, value) {
-                //find the footnotes at the bottom and extract the text
-                var _footnoteText_ = $(this).siblings(".endnote-text").text();
-                //var _bibCiteText_ = $(this).siblings(".endnote-text").text();
-                console.log(_footnoteText_);
-                //find the href of the related anchor from the id of this
-                var _id_ = $(this).attr("id");
-                console.log(_id_);
-                //use the id to fing the links href and insert the text from the 1st var
-                $('a[href$="' + _id_ + '"]').attr('data-toggle', 'tooltip').attr('data-original-title', _footnoteText_).attr('title', '');
-            }); */
-
-
 
             $.each($(".footnote-list ul.footnotes a"), function(index, value) {
                 //find the footnotes at the bottom and extract the text
@@ -35,20 +20,6 @@
                 //use the id to fing the links href and insert the text from the 1st var
                 $('a[href$="' + _id_ + '"]').attr('data-toggle', 'tooltip').attr('data-original-title', _footnoteText_).attr('title', '');
             });
-
-
-
-
-            //$.each($("li.footnote"), function(index, value) {
-                ////find the footnotes at the bottom and extract the text
-                //var _footnoteText_ = $(value).text();
-                //console.log(_footnoteText_);
-                ////find the href of the related anchor from the id of this
-                //var _id_ = $('.footnote-link', this).attr("id");
-                //console.log(_id_);
-                ////use the id to fing the links href and insert the text from the 1st var
-                //$('a[href$="' + _id_ + '"]').attr('data-toggle', 'tooltip').attr('data-original-title', _footnoteText_).attr('title', '');
-            //});
         }
     };
 
@@ -83,14 +54,6 @@
     };
 
 
-
-
-    Drupal.behaviors.article_collection_copier = {
-        attach: function attach(context, settings) {
-
-            $('.field--name-field-collection').appendTo("#toc-sections ol");
-        }
-    };
     Drupal.behaviors.article_keyword_copier = {
         attach: function attach(context, settings) {
 
@@ -114,7 +77,7 @@
         attach: function attach(context, settings) {
 
 
-            $('.toc a').on('click',function(e) {
+            $('.toc li a').on('click',function(e) {
                 e.preventDefault();
                 var offset = 100;
                 var target = this.hash;
